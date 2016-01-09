7b9d17052b58a670c46a93a5cd1b806f28b33eeb
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-09 01:04:05 -0500

    Initial Commit

---
+++ BlogReader3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlogReader3
+//
+//  Created by Kerry Toonen on 2016-01-09.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
